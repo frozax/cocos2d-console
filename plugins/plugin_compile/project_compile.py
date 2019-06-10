74b1d5c111ebf58d9f70684023b642dae52775e3
Francois Guibert <frozax@frozax.com>
2019-06-10 16:23:59 +0200

    bundle build
    bundle build

--- plugins/plugin_compile/project_compile.py
+++ plugins/plugin_compile/project_compile.py
@@ -82,6 +82,8 @@ class CCPluginCompile(cocos.CCPlugin):
                            help=MultiLanguage.get_string('COMPILE_ARG_NO_APK'))
         group.add_argument("--no-sign", dest="no_sign", action="store_true",
                            help=MultiLanguage.get_string('COMPILE_ARG_NO_SIGN'))
+        group.add_argument("--bundle", dest="bundle", action="store_true",
+                           help="Buid bundle instead of apk")
 
         group = parser.add_argument_group(MultiLanguage.get_string('COMPILE_ARG_GROUP_WIN'))
         group.add_argument("--vs", dest="vs_version", type=int,
@@ -537,7 +539,7 @@ class CCPluginCompile(cocos.CCPlugin):
         # build apk
         if not self._no_apk:
             cocos.Logging.info(MultiLanguage.get_string('COMPILE_INFO_BUILD_APK'))
-        self.apk_path = builder.do_build_apk(build_mode, self._no_apk, self._no_sign, output_dir, self._custom_step_args, self._ap, self)
+        self.apk_path = builder.do_build_apk(build_mode, self._no_apk, self._no_sign, output_dir, self._custom_step_args, self._ap, self, self._bundle)
         self.android_package, self.android_activity = builder.get_apk_info()
 
         cocos.Logging.info(MultiLanguage.get_string('COMPILE_INFO_BUILD_SUCCEED'))
